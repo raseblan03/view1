720d61ed3a17e926a2cf7f6b60b679bd3e6fe4eb
raseblan <100007877.joan23@fje.edu>
2022-02-02 11:45:05 +0100

    first commit

---
+++ mostrar.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Recogida de datos</title>
+</head>
+<body>
+   <h1>Recogida de datos</h1> 
+   <?php 
+   /*codigo php */
+echo '<p>'.$_GET['nombre'] .' </p>';
+echo $_GET['pwd'];
+   ?>
+</body>
+</html>
